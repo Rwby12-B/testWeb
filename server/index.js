d87cdc4cd6e09687b594f1e58f4aa2787632655f
Rwby12-B <ganaatemka614@gmail.com>
2026-04-21 00:56:51 +0800

    Change frontend route to serve index.html from root

--- server/index.js
+++ server/index.js
@@ -101,8 +101,8 @@ app.get('/api/reservations', (req, res) => {
 });
 
 // Serve frontend for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../index.html"));
 });
 
 // Start server
