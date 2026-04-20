041129781dfa8a2db983d59f8952fe826214d819
Rwby12-B <ganaatemka614@gmail.com>
2026-04-21 00:40:41 +0800

    Fix static file serving path in index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../")));
 
 // Helper: read JSON data file
 function readData(filename) {
